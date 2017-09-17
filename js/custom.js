89d40496b71de93780f5b6d4e9c1ff169fa85ed8
test4spa <31935865+test4spa@users.noreply.github.com>
2017-09-17 21:53:09 +0300

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,11 @@
 $(document).ready(function()
 {
+	$('.fa-stack').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $(this).attr('href') ).offset().top
+	    }, 500);
+	    return false;
+	});
 	$(window).scroll(function ()
 	{
 		if ($(window).scrollTop() > 45)
@@ -11,4 +17,23 @@ $(document).ready(function()
 	  		$('nav').removeClass('navbar-animation-onscroll');
 		}
 	});
+	$('.sidemenu').click(function(event)
+	{
+		event.stopPropagation();
+		if ($(this).hasClass('clicked'))
+		{
+			$(this).removeClass('clicked');
+			$('.desktop-visible').removeClass('sidenav-visible');
+		}
+		else
+		{
+			$(this).addClass('clicked');
+			$('.desktop-visible').addClass('sidenav-visible');
+		}
+	});
+	/*$('body').click(function()
+	{
+			$('.sidemenu').removeClass('clicked');
+			$('.desktop-visible').removeClass('sidenav-visible');
+	});*/
 });
