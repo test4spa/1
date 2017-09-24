fac69c045426e9a97cc2a27fa895a3ad40ebbd3e
test4spa <31935865+test4spa@users.noreply.github.com>
2017-09-24 22:02:06 +0300

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,39 @@
 $(document).ready(function()
-{$('.fa-stack').click(function(){$('html, body').animate({scrollTop:$($(this).attr('href')).offset().top},500);return false;});$(window).scroll(function()
-{if($(window).scrollTop()>45)
-{$('nav').addClass('navbar-animation-onscroll');}
-if($(window).scrollTop()<45)
-{$('nav').removeClass('navbar-animation-onscroll');}});$('.sidemenu').click(function(event)
-{event.stopPropagation();if($(this).hasClass('clicked'))
-{$(this).removeClass('clicked');$('.desktop-visible').removeClass('sidenav-visible');}
-else
-{$(this).addClass('clicked');$('.desktop-visible').addClass('sidenav-visible');}});});
+{
+	$('.fa-stack').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $(this).attr('href') ).offset().top
+	    }, 500);
+	    return false;
+	});
+	$(window).scroll(function ()
+	{
+		if ($(window).scrollTop() > 45)
+		{
+	  		$('nav').addClass('navbar-animation-onscroll');
+		}
+		if ($(window).scrollTop() < 45)
+		{
+	  		$('nav').removeClass('navbar-animation-onscroll');
+		}
+	});
+	$('.sidemenu').click(function(event)
+	{
+		event.stopPropagation();
+		if ($(this).hasClass('clicked'))
+		{
+			$(this).removeClass('clicked');
+			$('.desktop-visible').removeClass('sidenav-visible');
+		}
+		else
+		{
+			$(this).addClass('clicked');
+			$('.desktop-visible').addClass('sidenav-visible');
+		}
+	});
+	/*$('body').click(function()
+	{
+			$('.sidemenu').removeClass('clicked');
+			$('.desktop-visible').removeClass('sidenav-visible');
+	});*/
+});
